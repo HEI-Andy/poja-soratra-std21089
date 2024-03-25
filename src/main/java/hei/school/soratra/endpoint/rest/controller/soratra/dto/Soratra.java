ec936c68c5f902600d866ed54ccbc0c5cdaf1bec
gitdeverstein <hei.andy.8@gmail.com>
2024-03-25 09:59:33 +0300

    feat: add PUT /soratra/{id} and GET /soratra/{id}

---
+++ src/main/java/hei/school/soratra/endpoint/rest/controller/soratra/dto/Soratra.java
@@ -0,0 +1,14 @@
+package hei.school.soratra.endpoint.rest.controller.soratra.dto;
+
+import java.net.URL;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Soratra {
+    private URL original_url;
+    private URL transformed_url;
+}
